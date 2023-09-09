850dcba771a83204bc2837a7b217c3808cf59824
Sungwon Lee <sung1lee@kaist.ac.kr>
2023-09-09 03:49:56 +0000

    rename: 파일 이름 rename

---
+++ poster.py
@@ -0,0 +1,9 @@
+'''A번 문제'''
+
+y_len, x_len = [int(i) for i in input().split(' ')]
+poster_map = [list(input()) for _ in range(y_len)]
+
+for i in range(y_len):
+    if(poster_map[x_len // 2] != 'Y'
+
+print(poster_map)
